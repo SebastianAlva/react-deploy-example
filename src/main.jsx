5963c5b9d9d8b05ab5c8d56cbde691727b16e3ab
Sebastian Alva <98499668+SebastianAlva@users.noreply.github.com>
2024-03-11 20:17:32 -0400

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { TaskContextProvider } from "./context/TaskContext.jsx";
+import "./index.css"
+/* import body from "./components/styles.css" */
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <TaskContextProvider>
+      <App />
+    </TaskContextProvider>
+  </React.StrictMode>
+);
